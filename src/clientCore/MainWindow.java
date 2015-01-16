f632ad5c745aa5676a3ca3187326fc1e5a847fab
jklinin <yuri.kalinin@outlook.de>
2015-01-16 15:39:38 +0100

    fixed bug with MainWindow(GUI)

--- src/clientCore/MainWindow.java
+++ src/clientCore/MainWindow.java
@@ -1,14 +1,11 @@
 package clientCore;
 
-import javax.mail.Message;
+
 import javax.mail.MessagingException;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,22 +15,25 @@ import java.util.ArrayList;
 
 class MainWindow extends JFrame {
 	private DefaultTableModel model;
-	private static String passwordMail;
-	private JPanel buttonCont = new JPanel();
 	private JButton buttonNewMail;
 	private JButton buttonUpdateMail;
 	private JButton buttonAnswer;
 	private JButton buttonAddressBook;
 	private JTable previewMail;
 	private JEditorPane viewMail;
-	private JScrollPane buttonJsp;
+	
 	private JScrollPane scrollPane1;
 	private JScrollPane scrollPane2;
 	private JScrollPane scrollPane3;
+	private JPanel contenPanelSouth;
+	private JPanel buttonsPanel;
+	private JPanel contentPanelLeft;
+	private JPanel contentPanelRight;
+	
 	private JLabel previewlabel;
 	private JLabel viewlabel;
 	private JLabel statuslabel;
-	private Message[] messages = null;
+	
 	private ArrayList<MessagesDate> messagesList;
 	private String ccStringTemp;
 	private String bccStringString;
@@ -41,46 +41,61 @@ class MainWindow extends JFrame {
 	private String fromStringTemp;
 	private String subjectStringTemp;
 	private String sentDateStringTemp;
+	private static String passwordMail;
 	int selectedRow;
 
 	public MainWindow() {
 		super("Mail Client");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1500, 800);
 		addComponentsToPane();
 		setVisible(true);
 
 	}
 
-
 	public void addComponentsToPane() {
 		model = new DefaultTableModel() {
-    	@Override
-    	public boolean isCellEditable(int row, int column) {
-       		return false;
-    	}
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
 		};
 		previewMail = new JTable(model);
-		buttonCont = new JPanel();
-		buttonJsp = new JScrollPane(buttonCont);
 		buttonNewMail = new JButton();
 		buttonUpdateMail = new JButton();
 		buttonAnswer = new JButton();
 		buttonAddressBook = new JButton();
 		viewMail = new JEditorPane();
+		
 		scrollPane1 = new JScrollPane();
 		scrollPane2 = new JScrollPane();
 		scrollPane3 = new JScrollPane();
+		buttonsPanel = new JPanel();
+		contenPanelSouth = new JPanel();
+		contentPanelLeft = new JPanel();
+		contentPanelRight = new JPanel();
+		
 		previewlabel = new JLabel();
 		viewlabel = new JLabel();
 		statuslabel = new JLabel();
 		messagesList = new ArrayList();
-		Container gcp = getContentPane();
-		gcp.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		((GridBagLayout) gcp.getLayout()).columnWidths = new int[] { 800, 400, 400, 0 };
-		((GridBagLayout) gcp.getLayout()).rowHeights = new int[] { 0, 0, 600, 40, 0 };
+		
 
+		contentPanelLeft = new JPanel();
+		contentPanelLeft.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		contentPanelLeft.setLayout(new GridBagLayout());
+		((GridBagLayout) contentPanelLeft.getLayout()).columnWidths = new int[] { 0, 0 };
+		((GridBagLayout) contentPanelLeft.getLayout()).rowHeights = new int[] { 0, 0, 0, 0 };
+		((GridBagLayout) contentPanelLeft.getLayout()).columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		((GridBagLayout) contentPanelLeft.getLayout()).rowWeights = new double[] { 00.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
+		contentPanelRight = new JPanel();
+		contentPanelRight.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		contentPanelRight.setLayout(new GridBagLayout());
+		((GridBagLayout) contentPanelRight.getLayout()).columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		((GridBagLayout) contentPanelRight.getLayout()).rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+		((GridBagLayout) contentPanelLeft.getLayout()).columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		((GridBagLayout) contentPanelRight.getLayout()).rowWeights = new double[] { 00.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		buttonNewMail.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == buttonNewMail) {
@@ -91,15 +106,11 @@ class MainWindow extends JFrame {
 		});
 
 		buttonNewMail.setText("New Mail");
-		gcp.add(buttonJsp, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
-				GridBagConstraints.NORTH,
-				GridBagConstraints.VERTICAL,
-				new Insets(0, 0, 0, 0), 0, 0));
-		buttonCont.add(buttonNewMail);
+
+		buttonsPanel.add(buttonNewMail);
 
 		// --- button Answer EMail------------------------
-		c.gridx = 1;
-		c.gridy = 0;
+
 		buttonAnswer.setText("Answer");
 		buttonAnswer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -108,29 +119,27 @@ class MainWindow extends JFrame {
 				}
 			}
 		});
-		buttonCont.add(buttonAnswer);
+		buttonsPanel.add(buttonAnswer);
 
 		// -------------------------------------------------
 		buttonUpdateMail.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == buttonUpdateMail) {
-						try{
+					try {
 						passwordDialog();
-						}catch(NullPointerException ex){
-							System.err.println(ex.getMessage());
-							return;
-						}
-						new Thread(new UpdateEMailThread()).start();
+					} catch (NullPointerException ex) {
+						System.err.println(ex.getMessage());
+						return;
+					}
+					new Thread(new UpdateEMailThread()).start();
 
 				}
 			}
 
 		});
 
-		c.gridx = 2;
-		c.gridy = 0;
 		buttonUpdateMail.setText("Update");
-		buttonCont.add(buttonUpdateMail);
+		buttonsPanel.add(buttonUpdateMail);
 
 		buttonAddressBook.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -139,35 +148,24 @@ class MainWindow extends JFrame {
 				}
 			}
 		});
-		c.gridx = 3;
-		c.gridy = 0;
+
 		buttonAddressBook.setText("Address Book");
-		buttonCont.add(buttonAddressBook);
-
-
-
-		previewlabel.setText("Incoming mail");
-		gcp.add(previewlabel, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
-				GridBagConstraints.CENTER,
-				GridBagConstraints.VERTICAL,
-				new Insets(0, 0, 0, 0), 0, 0));
-		viewlabel.setText("Mail content");
-		c.gridx = 1;
-		c.gridy = 1;
-		c.gridwidth = 2;
-		c.gridheight = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.NONE;
-		gcp.add(viewlabel, c);
+		buttonsPanel.add(buttonAddressBook);
+		// --------------------------JLabels--------------------
+		previewlabel.setText("Incoming mail"+"          ");
+		previewlabel.setFont(new Font("Tahoma", Font.PLAIN, 16));		
+		buttonsPanel.add(previewlabel, 0);
+		viewlabel.setText("          "+"Mail content");
+		viewlabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		buttonsPanel.add(viewlabel, 5);
+		//-----------------------------------------------------
 		// ----Jtable----------------
-		scrollPane3.setViewportView(previewMail);
-		viewMail.setEditable(false); // set JEdotorPane not editable
 		model.addColumn("FROM");
 		model.addColumn("TO");
 		model.addColumn("Subject");
 		model.addColumn("Sent Date");
 		previewMail.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
+		contentPanelLeft.add(previewMail, new GridBagConstraints(0, 1, 0, 0, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(25, 0, 0, 0), 0, 0));
 		ListSelectionModel rowSM = previewMail.getSelectionModel();
 		rowSM.addListSelectionListener(new ListSelectionListener() {
 
@@ -208,13 +206,7 @@ class MainWindow extends JFrame {
 					}
 					subjectStringTemp = messagesList.get(selectedRow).getSubject();
 					sentDateStringTemp = messagesList.get(selectedRow).getSentDate().toString();
-					System.out.println(messagesList.get(selectedRow).getTypeMessages() + " type of selected messages");// TODO
-																														// remove
-																														// this
-																														// is
-																														// just
-																														// for
-																														// testing
+				
 					if (messagesList.get(selectedRow).getTypeMessages().equals("text")) {
 						viewMail.setContentType("text");
 						viewMail.setText("From: " + fromStringTemp + "\n" + "To: " + toStringTemp + "\n" + "CC: " + ccStringTemp + "\n" + "BCC: " + bccStringString + "\n" + "Subject: " + subjectStringTemp + "\n" + "Sent Date: " + sentDateStringTemp + "\n" + "\n"
@@ -224,52 +216,46 @@ class MainWindow extends JFrame {
 						viewMail.setText("<p> From: " + fromStringTemp + "<br>" + "To: " + toStringTemp + "<br>" + "CC: " + ccStringTemp + "<br>" + "BCC: " + bccStringString + "<br>" + "Subject: " + subjectStringTemp + "<br>" + "Sent Date: " + sentDateStringTemp + "<br>" + "</p>"
 								+ messagesList.get(selectedRow).getContent());
 					}
+					
+				
+				}
+				
+				if(viewMail.getSize().getHeight()<1792){
+					viewMail.setPreferredSize(new Dimension (700, 700));
+				}else{
+					viewMail.setPreferredSize(new Dimension(700, 1792));
 				}
 			}
 		});
 
 		for (int i = 0; i < 4; i++) {
-			previewMail.getColumnModel().getColumn(i).setPreferredWidth(200);
-			previewMail.getColumnModel().getColumn(i).setMaxWidth(200);
-			previewMail.getColumnModel().getColumn(i).setMinWidth(200);
+			previewMail.getColumnModel().getColumn(i).setPreferredWidth(180);
+			previewMail.getColumnModel().getColumn(i).setMaxWidth(180);
+			previewMail.getColumnModel().getColumn(i).setMinWidth(100);
 		}
-		c.gridx = 0;
-		c.gridy = 2;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.BOTH;
-		gcp.add(scrollPane3, c);
+
 		// ---------------------------------------------------
 		// thread for the adding of rows
 		new Thread(new AddRowsThread()).start();
-
 		// ----------JEditorPane----------------------------
-		scrollPane2.setViewportView(viewMail);
-		c.gridx = 1;
-		c.gridy = 2;
-		c.gridwidth = 2;
-		c.gridheight = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.BOTH;
-		gcp.add(scrollPane2, c);
-		// -------------------------------------------------
+		viewMail.setEditable(false); // set JEdotorPane not editable
 
+		viewMail.setPreferredSize(new Dimension(700, 700));
+		contentPanelRight.add(viewMail, new GridBagConstraints(0, 1, 0, 0, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(10, 0, 0, 0), 0, 0));
+		// -------------------------------------------------
 		statuslabel.setText("Status Bar");
-		c.gridx = 0;
-		c.gridy = 3;
-		c.gridwidth = 3;
-		c.gridheight = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.VERTICAL;
-		gcp.add(statuslabel, c);
-		/*
-		 * gcp.add(statuslabel, new GridBagConstraints(0, 3, 3, 1, 0.0, 0.0,
-		 * GridBagConstraints.CENTER, GridBagConstraints.VERTICAL, new Insets(0,
-		 * 0, 0, 0), 0, 0));
-		 */
-		pack();
+		statuslabel.setPreferredSize(new Dimension(160, 15));
+		contenPanelSouth.add(statuslabel);
+		//-----------Constraints---------------------------
+		getContentPane().add(buttonsPanel, BorderLayout.NORTH);
+		scrollPane1 = new JScrollPane(contentPanelLeft);
+		getContentPane().add(scrollPane1, BorderLayout.WEST);
+		scrollPane2 = new JScrollPane(contentPanelRight);
+		getContentPane().add(scrollPane2, BorderLayout.CENTER);
+		scrollPane3=new JScrollPane(contenPanelSouth);
+		getContentPane().add(scrollPane3, BorderLayout.SOUTH);
 		setLocationRelativeTo(getOwner());
+		//-------------------------------------------------
 	}
 
 	public static void passwordDialog() {
@@ -277,7 +263,7 @@ class MainWindow extends JFrame {
 		passwordField.setEchoChar('*');
 		JOptionPane.showMessageDialog(null, passwordField, "Enter password", JOptionPane.INFORMATION_MESSAGE);
 		passwordMail = String.valueOf(passwordField.getPassword());
-		if (passwordMail.equals("")){
+		if (passwordMail.equals("")) {
 			throw new NullPointerException("The field password is empty");
 		}
 	}
@@ -305,8 +291,8 @@ class MainWindow extends JFrame {
 		@Override
 		public void run() {
 
- 			try {
- 				addNewRowTable();
+			try {
+				addNewRowTable();
 			} catch (FileNotFoundException e) {
 				// this ok hear
 			} catch (ClassNotFoundException e) {
@@ -314,10 +300,10 @@ class MainWindow extends JFrame {
 			} catch (IOException e) {
 				System.err.println(e.getMessage());
 
- 			}
+			}
 
- 			statuslabel.setText("");
- 		}
+			statuslabel.setText("");
+		}
 	}
 
 	/**
@@ -336,10 +322,10 @@ class MainWindow extends JFrame {
 			} catch (MessagingException e) {
 				System.err.println(e.getMessage());
 
- 			}
-			try{
- 			readMail.getMassagesArray();
-			}catch(NullPointerException ex){
+			}
+			try {
+				readMail.getMassagesArray();
+			} catch (NullPointerException ex) {
 				statuslabel.setText("");
 				return;
 			}
