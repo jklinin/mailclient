d173ac4507e50fc849be9e3419936e43a4e06ef4
Nikolay Antonov <nikant92@gmail.com>
2015-01-17 10:56:35 +0100

    #working table

--- src/clientCore/AddressBook.java
+++ src/clientCore/AddressBook.java
@@ -14,14 +14,13 @@ public class AddressBook extends JFrame {
     private JPanel buttonCont = new JPanel();
     private JScrollPane jsp = new JScrollPane();
     private JScrollPane jsp2 = new JScrollPane();
-    private JEditorPane table;
     private JTable viewBook;
 
 
 
 
     public AddressBook() {
-		super("New Mail");
+		super("Address Book");
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setSize(600, 641);
 		initialization();
@@ -39,7 +38,7 @@ public class AddressBook extends JFrame {
         buttonCont = new JPanel();
         jsp = new JScrollPane(buttonCont);
         jsp2 = new JScrollPane();
-        viewBook = new JTable();
+        viewBook = new JTable(model);
         Container gcp = getContentPane();
 		gcp.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
@@ -59,14 +58,19 @@ public class AddressBook extends JFrame {
 		buttonCont.add(buttonRemove);
 
         jsp2.setViewportView(viewBook);
-    	model.addColumn("FROM");
-    	model.addColumn("TO");
-    	model.addColumn("Subject");
-    	model.addColumn("Sent Date");
+    	model.addColumn("Name");
+    	model.addColumn("Surname");
+    	model.addColumn("Email_addr");
+		for (int i = 0; i < 3; i++) {
+			viewBook.getColumnModel().getColumn(i).setPreferredWidth(200);
+			viewBook.getColumnModel().getColumn(i).setMaxWidth(200);
+			viewBook.getColumnModel().getColumn(i).setMinWidth(100);
+		}
 		gcp.add(jsp2, new GridBagConstraints(0, 1, 3, 1, 0.0, 0.0,
 				GridBagConstraints.CENTER,
 				GridBagConstraints.BOTH,
 				new Insets(0, 0, 0, 0), 0, 0));
+
 
     }
 }
